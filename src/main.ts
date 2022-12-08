98003c5a2aeeb01f8c050bc764e7bfbf52011c89
Sam Van der Plaetsen <samvande12@student.artevelehs.be>
2022-12-08 15:40:50 +0100

    [WIP]

--- src/main.ts
+++ src/main.ts
@@ -6,3 +6,18 @@ const login = new LoginComponent();
 const appContainer = document.querySelector<HTMLDivElement>('#app')!;
 
 appContainer.appendChild(login.render());
+
+// submit button nog html and css maken
+
+const initApp = () => {
+  const $submitButton = document.querySelector('#login');
+
+  $submitButton?.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log('test');
+    // firebase login code
+    // login geslaagd loggen fzo en firebase fixen
+  });
+};
+
+window.addEventListener('load', initApp);
